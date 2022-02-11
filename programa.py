0ab62f1faf6c20fc0dfe3530bb49efbc1cabb0a1
Ana San <67754798+eusanchez@users.noreply.github.com>
2022-02-11 14:21:33 -0600

    Add files via upload
    Word reverse with input

---
+++ programa.py
@@ -0,0 +1,10 @@
+#programa para imprimir una palabra a lo inverso.
+#Ana Eugenia Sanchez V.
+
+usuario = input("Digite una palabra: ")
+
+lista = list(usuario)
+lista.reverse()
+print ("Esta es mi palabra en reversa: ", "".join(lista)) #palabra reversa
+
+print("Esta es mi palabra original: ",usuario)
